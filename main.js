99cb8a174643397bf25526bc8fc6057572f09030
UKISTU18 <arajegan.aj@gmail.com>
2019-02-01 12:58:15 +0530

    exee

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ SC.initialize({
 });
 
 $(document).ready(function() {
- SC.stream('/tracks/481417257',function(sound){ });
+ SC.stream('/tracks/291358852',function(sound){ });
           $('#start').click(function(e) {
             e.preventDefault();
             sound.start();

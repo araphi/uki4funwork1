3a6bb9a69ab3a15928369f1ad45b3225b1779217
UKISTU18 <arajegan.aj@gmail.com>
2019-02-11 10:27:49 +0530

    fun

--- main.js
+++ main.js
@@ -2,14 +2,95 @@ SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
 
-$(document).ready(function() {
- SC.stream('/tracks/291358852',function(sound){ });
-          $('#start').click(function(e) {
-            e.preventDefault();
-            sound.start();
-          });
-          $('#stop').click(function(e) {
-                     e.preventDefault();
-                     sound.stop();
-                   });
+
+$(document).ready(function()   {
+
+  SC.stream('/tracks/540186672 ', function(sound) {
+  $('#start1').click(function(e) {
+    e.preventDefault();
+    sound.start();
   });
+  $('#stop1').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+// sarvam thaalamayam
+ SC.stream('/tracks/310866390', function(sound) {
+  $('#start2').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#stop2').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+// kadal
+ SC.stream('/tracks/299754917', function(sound) {
+  $('#start3').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#stop3').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+// rasaliye
+SC.stream('/tracks/568670007', function(sound) {
+ $('#start4').click(function(e) {
+   e.preventDefault();
+   sound.start();
+ });
+ $('#stop4').click(function(e) {
+   e.preventDefault();
+   sound.stop();
+ });
+});
+// neyum nanum anbeh
+SC.stream('/tracks/295549427', function(sound) {
+ $('#start5').click(function(e) {
+   e.preventDefault();
+   sound.start();
+ });
+ $('#stop5').click(function(e) {
+   e.preventDefault();
+   sound.stop();
+ });
+});
+// manusla poongathe
+SC.stream('/tracks/331502284', function(sound) {
+ $('#start6').click(function(e) {
+   e.preventDefault();
+   sound.start();
+ });
+ $('#stop6').click(function(e) {
+   e.preventDefault();
+   sound.stop();
+ });
+});
+// neyum naum
+SC.stream('/tracks/73473040', function(sound) {
+ $('#start7').click(function(e) {
+   e.preventDefault();
+   sound.start();
+ });
+ $('#stop7').click(function(e) {
+   e.preventDefault();
+   sound.stop();
+ });
+});
+// kanave kanave
+SC.stream('/tracks/303954855', function(sound) {
+ $('#start8').click(function(e) {
+   e.preventDefault();
+   sound.start();
+ });
+ $('#stop8').click(function(e) {
+   e.preventDefault();
+   sound.stop();
+ });
+});
+// sethupathi
+});
